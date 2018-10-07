b30816626b47c448e51e3eb846740f8f50e1b660
peter-keller <keller.f.peter@gmail.com>
2018-10-07 23:16:55 +0100

    footer created

--- quarr/src/App.js
+++ quarr/src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 import "./Assets/bootstrap/css/bootstrap.min.css";
 import AppNavbar from "./Components/Navigation/Navigation";
+import Footer from "./Components/Footer/Footer";
 
 class App extends Component {
   render() {
-    return <AppNavbar />;
+    return (
+      <React.Fragment>
+        <AppNavbar />
+        <Footer />
+      </React.Fragment>
+    );
   }
 }
 
