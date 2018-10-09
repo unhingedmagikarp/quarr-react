8f99e6ed84db5623867cb144a611de2dfc141221
peter-keller <keller.f.peter@gmail.com>
2018-10-09 23:21:01 +0100

    landingpicture component

--- quarr/src/App.js
+++ quarr/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import "./Assets/bootstrap/css/bootstrap.min.css";
 import AppNavbar from "./Components/Navigation/Navigation";
 import Footer from "./Components/Footer/Footer";
+import LandingPicture from "./Components/LandingPage/LandingPicture";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <AppNavbar />
+        <LandingPicture />
         <Footer />
       </React.Fragment>
     );
---
+++ quarr/src/Components/LandingPage/LandingPicture.js
@@ -0,0 +1,31 @@
+import React from "react";
+import backgroundPic from "../../Assets/img/tech/image4.jpeg";
+
+const LandingPicture = () => {
+  return (
+    <section
+      className="clean-block clean-hero"
+      style={{
+        backgroundImage: backgroundPic,
+        backgroundRepeat: "no-repeat",
+        color: "rgba(0, 0, 0, 0.35)"
+      }}
+    >
+      <div className="text">
+        <h2>Paintings, photographs and other artwork in Swanage, Dorset</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam
+          urna, dignissim nec auctor in, mattis vitae leo.
+        </p>
+        <button
+          className="btn btn-outline-light btn-lg"
+          onClick={window.scrollBy({ top: 10, left: 0, behavior: "smooth" })}
+        >
+          Learn More
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default LandingPicture;
