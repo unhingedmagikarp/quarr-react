385d4b1db056cbb48c116884613be5abecdc5a2e
peter-keller <keller.f.peter@gmail.com>
2018-11-01 14:00:09 +0000

    meta tag v1, landing image

--- quarr/src/Components/LandingPage/LandingPage.js
+++ quarr/src/Components/LandingPage/LandingPage.js
@@ -2,10 +2,24 @@ import React from "react";
 import LandingPicture from "./LandingPicture";
 import AboutUs from "./AboutUs";
 import Slider from "./SliderComponent";
+import MetaTags from "react-meta-tags";
+import MetaImage from "../../Assets/img/ls.jpg";
 
 const LandingPage = () => {
   return (
     <React.Fragment>
+      <MetaTags>
+        <title>Home - Quarr Gallery</title>
+        <meta
+          name="description"
+          content="QUARR Gallery shows work by a number of artists and photographers based in and around Swanage in Dorset."
+        />
+        <meta property="og:title" content="Quarr Gallery in Swanage, Dorset" />
+        <meta property="og:image" content={MetaImage} />
+        <meta property="og:site_name" content="Quarr Gallery" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="http://www.quarrgallery.com/" />
+      </MetaTags>
       <LandingPicture />
       <AboutUs />
       <Slider />
